799ac161c271d4bc81f4a9cb24e18a4472b7092c
Minhly <minh.y.ly@hotmail.com>
2023-09-10 20:52:33 +0200

    Added react router and frontpage layout

--- gamblingsite/src/App.js
+++ gamblingsite/src/App.js
@@ -1,26 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
-import Button from '@mui/material/Button'
+import "./App.css";
+import Button from "@mui/material/Button";
+import { AppBar, Grid, Link, Toolbar, Typography } from "@mui/material";
+import AccessAlarmIcon from "@mui/icons-material/AccessAlarm";
+import { BrowserRouter as Route, Router, Switch } from 'react-router-dom';
+import Admin from "./Pages/admin";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          David Sucks!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Free Hentai Click Here!
-        </a>
-        <Button variant="contained">Hello world!</Button>
-      </header>
-    </div>
+    <Router>
+      <AppBar position="relative">
+        <Toolbar>
+          <Grid container padding={2}>
+            <Grid item md="10">
+              <Typography variant="h3" fontWeight="bold">
+                Gambleon
+              </Typography>
+            </Grid>
+            <Grid item md="2" align="right">
+              <Switch>
+                <Route path="/">
+                  <Admin />
+                </Route>
+              </Switch>
+              <AccessAlarmIcon fontSize="large" />
+            </Grid>
+          </Grid>
+        </Toolbar>
+      </AppBar>
+      <main>
+        <div>
+          <Button variant="outlined">Hello world!</Button>
+        </div>
+      </main>
+    </Router>
   );
 }
 
---
+++ gamblingsite/src/Pages/admin.js
@@ -0,0 +1,11 @@
+import { AppBar, Grid, Toolbar, Typography } from "@mui/material";
+
+function Admin() {
+  return (
+    <Typography variant="h3" align="center">
+      Admin
+    </Typography>
+  );
+}
+
+export default Admin;
