e2759086b985de60dee128cd74817cb91d6832ff
Minhly <minh.y.ly@hotmail.com>
2023-09-20 12:40:16 +0200

    Added Zustand

---
+++ gamblingsite/src/components/zustandStore.js
@@ -0,0 +1,16 @@
+import { create } from 'zustand'
+import { persist, createJSONStorage } from 'zustand/middleware'
+
+export const useLoggedInStore = create(
+  persist(
+    (set, get) => ({
+      isLoggedIn: false,
+      bearerToken: "",
+      setIsLoggedIn: (state) => set({ isLoggedIn: state }),
+      setBearerToken: (state) => set({ bearerToken: state }),
+    }),
+    {
+        name: 'isLoggedInStorage',
+    }
+  )
+)
